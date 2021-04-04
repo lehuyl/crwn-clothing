6e0b81715f3a545176af71666952a04403c6e638
Steven <stevenle18@gmail.com>
2021-04-03 23:13:30 -0700

    refactor(homepage) broke down into components

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Directory from '../../components/Directory/Directory';
+
+const HomePage = () => (
+    <div className="flex flex-col items-center py-20 px-80">
+        <Directory />
+    </div>
+);
+export default HomePage;
