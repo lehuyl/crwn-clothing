fd9fda3c5037201c201cee6dc0bc6f2b97059a30
Steven <stevenle18@gmail.com>
2021-04-04 19:27:51 -0700

    feat(home-page) added hover effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.scss';
 import HomePage from './pages/HomePage/HomePage';
 
 function App() {
--- src/components/MenuItem/MenuItem.tsx
+++ src/components/MenuItem/MenuItem.tsx
@@ -6,12 +6,16 @@ const MenuItem = ({ title, imageUrl, size }: Section) => {
     return (
         // MenuItem
         <div
-            className={`${size} menu-item flex flex-auto mx-2 mb-4 min-w-30 h-64 items-center justify-center border border-black bg-center bg-cover`}
-            style={{
-                backgroundImage: `url(${imageUrl})`,
-            }}
+            className={`${size} menu-item flex flex-auto mx-2 mb-4 min-w-30 h-64 items-center justify-center border border-black overflow-hidden`}
         >
-            <div className="flex flex-col h-20 px-4 items-center justify-center border border-black">
+            <div
+                className="h-full w-full bg-center bg-cover background-image"
+                style={{
+                    backgroundImage: `url(${imageUrl})`,
+                }}
+            ></div>
+            {/* Content */}
+            <div className="flex flex-col h-20 px-4 items-center justify-center border border-black bg-white bg-opacity-70 absolute hover:bg-opacity-90">
                 <h1 className="font-bold mb-2 text-2xl text-gray-500">
                     {title?.toUpperCase()}
                 </h1>
