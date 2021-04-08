721c739abf82cbd693e5876f48bc4504a122077f
Steven <stevenle18@gmail.com>
2021-04-07 21:11:14 -0700

    feat(header) added header to homepage and shop page
    Signed-off-by: Steven <stevenle18@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
 import './App.scss';
+import Header from './components/Header/Header';
 import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/Shop/ShopPage';
 
 function App() {
     return (
         <div>
+            <Header />
             <Switch>
                 <Route exact path="/" component={HomePage} />
                 <Route path="/shop" component={ShopPage} />
