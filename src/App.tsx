cf84ccb6825f191a07e29e83c6ef9f8342d82f73
Steven <stevenle18@gmail.com>
2021-04-08 21:49:15 -0700

    change to Formik forms
    Signed-off-by: Steven <stevenle18@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 import './App.scss';
 import Header from './components/Header/Header';
+import AuthenticationPage from './pages/Authentication/AuthenticationPage';
 import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/Shop/ShopPage';
 
@@ -12,6 +13,7 @@ function App() {
             <Switch>
                 <Route exact path="/" component={HomePage} />
                 <Route path="/shop" component={ShopPage} />
+                <Route path="/sign-in" component={AuthenticationPage} />
             </Switch>
         </div>
     );
