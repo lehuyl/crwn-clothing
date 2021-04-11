56a6d64ee8366b45a7a1ec7f3312af5d93ac0880
Steven <stevenle18@gmail.com>
2021-04-10 21:50:50 -0700

    feat(sign-in) removed Formik forms

--- src/components/CollectionPreview/CollectionPreview.tsx
+++ src/components/CollectionPreview/CollectionPreview.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { Item } from '../../common/types';
 import CollectionItem from '../CollectionItem/CollectionItem';
 
-const CollectionPreview = ({ title, items }: any) => (
+interface Props {
+    title: string;
+    items: Item[];
+}
+
+const CollectionPreview: React.FunctionComponent<Props> = ({ title, items }: Props) => (
     <div className="flex flex-col mb-4">
         <h1 className="text-2xl mb-4">{title}</h1>
         <div className="flex justify-between">
--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -1,89 +1,63 @@
 import React from 'react';
-import { Field, Form, Formik } from 'formik';
-import * as yup from 'yup';
-import './SignIn.scss';
+import CustomButton from '../CustomButton/CustomButton';
+import FormInput from '../FormInput/FormInput';
 
-const signinSchema = yup.object().shape({
-    email: yup
-        .string()
-        .email('Invalid email')
-        .required('Please enter valid email'),
-    password: yup
-        .string()
-        .required('Please enter password')
-        .min(8, 'Password is too short. 8 characters minimum'),
-});
+interface SignInState {
+    email: string;
+    password: string;
+}
 
-const SignIn = () => (
-    <div className="flex flex-col w-30vw">
-        <h2 className="text-2xl font-semibold text-gray-700">
-            I already have an account.
-        </h2>
-        <span className="text-gray-700 mb-2">
-            Sign in with your email and password.
-        </span>
+class SignIn extends React.Component<{}, SignInState> {
+    constructor(props: any) {
+        super(props);
 
-        <Formik
-            initialValues={{
-                email: '',
-                password: '',
-            }}
-            validationSchema={signinSchema}
-            onSubmit={() => {
-                console.log('sign in not yet configured');
-            }}
-        >
-            {({ values, errors, touched }) => (
-                <Form>
-                    <div className="group">
-                        <Field
-                            className="form-input"
-                            name="email"
-                            type="email"
-                            label="email"
-                        />
-                        <label
-                            className={`${values.email.length ? 'shrink' : ''} form-input-label`}
-                            htmlFor="email"
-                        >
-                            Email
-                        </label>
-                        {errors.email && touched.email ? (
-                            <div className="text-sm text-red-600">
-                                {errors.email}
-                            </div>
-                        ) : null}
-                    </div>
-                    <div className="group">
-                        <Field
-                            className="form-input"
-                            name="password"
-                            type="password"
-                            label="password"
-                        />
-                        <label
-                            className={`${values.password.length ? 'shrink' : ''} form-input-label`}
-                            htmlFor="password"
-                        >
-                            Password
-                        </label>
-                        {errors.password && touched.password ? (
-                            <div className="text-sm text-red-600">
-                                {errors.password}
-                            </div>
-                        ) : null}
-                    </div>
-                    <div>
-                        <input
-                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                            type="submit"
-                            value="Sign in"
-                        />
-                    </div>
-                </Form>
-            )}
-        </Formik>
-    </div>
-);
+        this.state = {
+            email: '',
+            password: '',
+        };
+    }
+
+    handleSubmit = (event: any) => {
+        event.preventDefault();
+        this.setState({ email: '', password: '' });
+    };
+
+    handleChange = (event: any) => {
+        const { value, name } = event.target;
+        // @ts-ignore
+        this.setState({ [name]: value });
+    };
+
+    render() {
+        return (
+            <div className="flex flex-col w-30vw">
+                <h2 className="text-2xl font-semibold text-gray-700">
+                    I already have an account.
+                </h2>
+                <span>Sign in with your email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput
+                        name="email"
+                        type="email"
+                        handleChange={this.handleChange}
+                        value={this.state.email}
+                        label="email"
+                        required
+                    />
+                    <FormInput
+                        name="password"
+                        type="password"
+                        value={this.state.password}
+                        handleChange={this.handleChange}
+                        label="password"
+                        required
+                    />
+                    <CustomButton type='submit'> Sign in </CustomButton>
+                </form>
+            </div>
+        );
+    }
+}
 
 export default SignIn;
