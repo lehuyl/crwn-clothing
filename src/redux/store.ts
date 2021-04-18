3b7d69a22657d3b9772511f1fef75d7bd9020896
Steven <stevenle18@gmail.com>
2021-04-17 18:05:50 -0700

    feat(redux) add currentUser to store with action and reducer
    Signed-off-by: Steven <stevenle18@gmail.com>

---
+++ src/redux/store.ts
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware, Store } from 'redux';
+import logger from 'redux-logger';
+import rootReducer from './root.reducer.';
+
+const middlewares = [logger];
+
+const store: Store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+
+export default store;
