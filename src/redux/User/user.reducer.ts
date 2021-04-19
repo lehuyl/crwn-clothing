d345a07e5a3967879d9151199474a84a44517a5c
Steven <stevenle18@gmail.com>
2021-04-18 18:35:00 -0700

    feat(redux) add redirect to homepage after sign in

--- src/redux/User/user.reducer.ts
+++ src/redux/User/user.reducer.ts
@@ -1,10 +1,12 @@
+import { UserActionTypes } from "./user.types";
+
 const INITIAL_USER_STATE = {
     currentUser: null
 }
 
 export const userReducer = (state = INITIAL_USER_STATE, action: any) => {
     switch (action.type) {
-        case 'SET_CURRENT_USER':
+        case UserActionTypes.SET_CURRENT_USER:
             return {
                 ...state,
                 currentUser:  action.payload
