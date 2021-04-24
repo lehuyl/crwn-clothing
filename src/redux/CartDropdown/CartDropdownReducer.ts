d25564ffea5e626a0b47faa3f5274f1fbc619303
Steven <stevenle18@gmail.com>
2021-04-23 21:20:27 -0700

    feat(cart) changed cart to use map and updated id of shop data
    Signed-off-by: Steven <stevenle18@gmail.com>

--- src/redux/CartDropdown/CartDropdownReducer.ts
+++ src/redux/CartDropdown/CartDropdownReducer.ts
@@ -1,29 +1,39 @@
-import { Item } from "../../common/types";
-import { CartDropdownTypes } from "./CartDropdownActions"
+import { CartItem } from '../../common/types';
+import { CartDropdownTypes } from './CartDropdownActions';
 
 interface CartDropdownState {
     isVisible: boolean;
-    cartItems: Item[];
+    cartItems: { [id: string]: CartItem };
 }
 
 const INITIAL_CART_DROPDOWN_STATE: CartDropdownState = {
     isVisible: false,
-    cartItems: []
-}
+    cartItems: {},
+};
 
-export const cartDropdownReducer = (state = INITIAL_CART_DROPDOWN_STATE, action: any) => {
+export const cartDropdownReducer = (
+    state = INITIAL_CART_DROPDOWN_STATE,
+    action: any,
+) => {
     switch (action.type) {
         case CartDropdownTypes.TOGGLE_CART_VISIBILITY:
             return {
                 ...state,
-                isVisible: !state.isVisible
-            }
+                isVisible: !state.isVisible,
+            };
         case CartDropdownTypes.ADD_ITEM:
-            return {
-                ...state,
-                cartItems: [...state.cartItems, action.payload]
-            }
+            let newState = { ...state };
+            const item = action.payload;
+            if (!state.cartItems[item.id])
+                newState.cartItems[item.id] = { item, quantity: 1 };
+            else
+                newState.cartItems[item.id] = {
+                    item,
+                    quantity: newState.cartItems[item.id].quantity + 1,
+                };
+
+            return newState;
         default:
             return state;
     }
-}
+};
