9ee77b0df672b52655ab9a2284038000689cebed
Steven <stevenle18@gmail.com>
2021-04-28 22:06:17 -0700

    feat(checkout page) add increase/decrease quantity to arrows

--- src/components/CheckoutItem/CheckoutItem.tsx
+++ src/components/CheckoutItem/CheckoutItem.tsx
@@ -1,35 +1,61 @@
 import React, { Dispatch } from 'react';
 import { connect } from 'react-redux';
 import { Action } from 'redux';
-import { Product } from '../../common/types';
-import { removeItem } from '../../redux/Cart/CartAction';
+import { Item, Product } from '../../common/types';
+import {
+    addItem,
+    clearItemFromCart,
+    removeItem,
+} from '../../redux/Cart/CartAction';
 
 import './CheckoutItem.scss';
 
 interface Props {
     cartItem: Product;
-    removeItem: (id: number) => Dispatch<Action>
+    addItem: (item: Item) => Dispatch<Action>;
+    removeItem: (id: number) => Dispatch<Action>;
+    clearItemFromCart: (id: number) => Dispatch<Action>;
 }
 
 const CheckoutItem = ({
-    cartItem: {
-        item: { id, name, imageUrl, price },
-        quantity,
-    }, removeItem
-}: Props) => (
-    <div className="checkout-item">
-        <div className="image-container">
-            <img src={imageUrl} alt="items" />
+    cartItem,
+    addItem,
+    removeItem,
+    clearItemFromCart,
+}: Props) => {
+    const { id, name, imageUrl, price } = cartItem.item;
+    const { quantity } = cartItem;
+
+    return (
+        <div className="checkout-item">
+            <div className="image-container">
+                <img src={imageUrl} alt="items" />
+            </div>
+            <span className="name">{name}</span>
+            <span className="quantity">
+                <div className="arrow" onClick={() => removeItem(id)}>
+                    &#10094;
+                </div>
+                <span className="value">{quantity}</span>
+                <div className="arrow" onClick={() => addItem(cartItem.item)}>
+                    &#10095;
+                </div>
+            </span>
+            <span className="price">{price}</span>
+            <div
+                className="remove-button"
+                onClick={() => clearItemFromCart(id)}
+            >
+                &#10005;
+            </div>
         </div>
-        <span className="name">{name}</span>
-        <span className="quantity">{quantity}</span>
-        <span className="price">{price}</span>
-        <div className="remove-button" onClick={() => removeItem(id)}>&#10005;</div>
-    </div>
-);
+    );
+};
 
 const mapDispatchToProps = (dispatch: any) => ({
-    removeItem: (id: number) => dispatch(removeItem(id))
-})
+    addItem: (item: Item) => dispatch(addItem(item)),
+    removeItem: (id: number) => dispatch(removeItem(id)),
+    clearItemFromCart: (id: number) => dispatch(clearItemFromCart(id)),
+});
 
 export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/redux/Cart/CartAction.ts
+++ src/redux/Cart/CartAction.ts
@@ -3,6 +3,7 @@ import { Item } from "../../common/types"
 export const CartTypes = {
     TOGGLE_CART_VISIBILITY: 'cart/toggleVisibility',
     ADD_ITEM: 'cart/addItem',
+    CLEAR_ITEM_FROM_CART: 'cart/clearItemFromCart',
     REMOVE_ITEM: 'cart/removeItem'
 }
 
@@ -15,7 +16,13 @@ export const addItem = (item: Item) => ({
     payload: item
 })
 
+// Only need map key to remove after it is added
 export const removeItem = (id: number) => ({
     type: CartTypes.REMOVE_ITEM,
+    payload: id
+})
+
+export const clearItemFromCart = (id: number) => ({
+    type: CartTypes.CLEAR_ITEM_FROM_CART,
     payload: id
 })
