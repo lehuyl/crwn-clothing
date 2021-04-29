e35e769d906d24da6b474b7dbd91e572e4583503
Steven <stevenle18@gmail.com>
2021-04-28 22:06:17 -0700

    feat(checkout page) add increase/decrease quantity to arrows

--- src/redux/Cart/CartReducer.ts
+++ src/redux/Cart/CartReducer.ts
@@ -18,20 +18,38 @@ export const cartReducer = (state = INITIAL_CART_STATE, action: any) => {
                 ...state,
                 isVisible: !state.isVisible,
             };
-        case CartTypes.ADD_ITEM:
-            const cartItems = {...state.cartItems};
+        case CartTypes.ADD_ITEM: {
+            const cartItems = { ...state.cartItems };
             const item = action.payload;
 
             cartItems[item.id] = cartItems[item.id]
                 ? { item, quantity: cartItems[item.id].quantity + 1 }
                 : { item, quantity: 1 };
-                
-            return {...state, cartItems};
-        case CartTypes.REMOVE_ITEM:
+
+            return { ...state, cartItems };
+        }
+        case CartTypes.REMOVE_ITEM: {
+            const id = action.payload;
+            const newCartItems = { ...state.cartItems };
+            const cartItem = { ...state.cartItems[id] };
+
+            if (cartItem.quantity > 1) {
+                cartItem.quantity--;
+                newCartItems[id] = cartItem;
+
+                return { ...state, cartItems: newCartItems };
+            } else {
+                const { [id]: _, ...otherCartIems } = state.cartItems;
+
+                return { ...state, cartItems: otherCartIems };
+            }
+        }
+        case CartTypes.CLEAR_ITEM_FROM_CART: {
             const id = action.payload;
             const { [id]: _, ...otherCartIems } = state.cartItems;
 
-            return {...state, cartItems: otherCartIems};
+            return { ...state, cartItems: otherCartIems };
+        }
         default:
             return state;
     }
