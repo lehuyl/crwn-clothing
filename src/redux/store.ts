e1fd0492ccd94fdd0c9b79a426f264898c9b0462
Steven <stevenle18@gmail.com>
2021-04-29 20:54:24 -0700

    feat(cart) add cart persistence
    Signed-off-by: Steven <stevenle18@gmail.com>

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,14 @@
 import { createStore, applyMiddleware, Store } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import rootReducer from './RootReducer';
+import { Persistor } from 'redux-persist/es/types';
 
 const middlewares = [logger];
-const store: Store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store: Store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+export const persistor: Persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch
-export default store;
