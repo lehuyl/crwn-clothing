4280f3a95f24b1fe9e108fefeeec2bfcccc68bd9
Steven <stevenle18@gmail.com>
2021-05-09 19:12:45 -0700

    feat(collections) fetch now done with redux thunk, and stop unnecessary collection call after remounting since no longer a subscription

--- src/redux/Shop/ShopAction.ts
+++ src/redux/Shop/ShopAction.ts
@@ -1,10 +1,44 @@
-import { Collection } from "../../common/types"
+import { Collection } from '../../common/types';
+import {
+    convertCollectionsSnapshotToMap,
+    firestore,
+} from '../../firebase/firebase.utils';
+import { AppDispatch, AppThunk } from '../store';
 
 export const ShopActionTypes = {
-    UPDATE_COLLECTION: 'shop/update-collection'
-}
+    FETCH_COLLECTIONS_START: 'shop/fetch-collections-start',
+    FETCH_COLLECTIONS_SUCCESS: 'shop/fetch-collections-success',
+    FETCH_COLLECTIONS_FAILURE: 'shop/fetch-collections-failure',
+};
 
-export const updateCollections = (collectionsMap: { [id: string]: Collection }) => ({
-    type: ShopActionTypes.UPDATE_COLLECTION,
-    payload: collectionsMap
-})
+export const fetchCollectionsStart = () => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_START,
+});
+
+export const fetchCollectionsSuccess = (collectionsMap: {
+    [id: string]: Collection;
+}) => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+    payload: collectionsMap,
+});
+
+export const fetchCollectionsFailure = (error: any) => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+    payload: error,
+});
+
+export const fetchCollectionsStartAsync = (): AppThunk => {
+    return (dispatch: AppDispatch) => {
+        const collectionRef = firestore.collection('collections');
+        dispatch(fetchCollectionsStart());
+        collectionRef
+            .get()
+            .then((snapshot: any) => {
+                const collectionsMap = convertCollectionsSnapshotToMap(
+                    snapshot,
+                );
+                dispatch(fetchCollectionsSuccess(collectionsMap));
+            })
+            .catch((error: any) => dispatch(fetchCollectionsFailure(error)));
+    };
+};
